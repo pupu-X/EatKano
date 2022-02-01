e526a94f77c806504b8866650c652bffb9685f3a
pupu-X <98788648+pupu-X@users.noreply.github.com>
2022-02-01 09:37:56 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
+        if (cps <= 5) return '好拉呀';
         if (cps <= 8) return 'TCL';
         if (cps <= 10)  return 'TQL';
         if (cps <= 15) return '您';
-        return '人？';
+        return '爹';
     }
 
     function toStr(obj) {
