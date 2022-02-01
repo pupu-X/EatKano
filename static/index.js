6ef933c9ac7583bbecdfb631319c631f26bb8e87
pupu-X <98788648+pupu-X@users.noreply.github.com>
2022-02-01 09:39:34 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -442,9 +442,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
 
         if (cps <= 5) return '好拉呀';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 8) return '再多一点？';
+        if (cps <= 10)  return '玛丽给你咬一口';
+        if (cps <= 15) return '挖';
         return '爹';
     }
 
